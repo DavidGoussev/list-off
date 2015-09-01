2ed9f71e190fee3334186928843a98768f1e9ba5
dpg5000 <dgoussev@gmail.com>
2015-08-31 22:53:26 -0700

    factory girl, rspec, item test pass

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -25,7 +25,10 @@
 
 FactoryGirl.define do
   factory :user do
-    
+    name "John Johnson"
+    sequence(:email, 100) { |n| "person#{n}@example.com" }
+    password "helloworld"
+    password_confirmation "helloworld"
+    confirmed_at Time.now
   end
-
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,6 +11,19 @@
 
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Item do
+
+    describe "create" do
+
+      before do
+        @user2 = create(:user)
+        2.times { create(:item, user: @user2) }
+      end
+
+      it "stores to-do item count on user2" do
+        expect( @user2.items.count ).to eq(2)
+      end
+
+
+  end
 end
